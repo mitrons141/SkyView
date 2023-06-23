1c030a7d7e19e2985f67596342361c7558b0b433
mitrons141 <mitrajeetdas01@gmail.com>
2023-06-24 04:28:49 +0530

    updated:styling

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -19,7 +19,7 @@ const Air = ({ datas }) => {
   const aqiCategory = getAqiCategory(main.aqi);
 
   return (
-    <div className="border border-gray-700 rounded-[1rem] bg-transparent w-[31rem] p-6 absolute left-[29rem] top-[11.6rem]">
+    <div className="border border-gray-700 rounded-[1rem] backdrop-blur-xl w-[31rem] p-6 absolute left-[29rem] top-[11.6rem]">
       <div className="flex justify-between items-center">
         <p className="text-slate-50">Air Quality Index</p>
         <p className="border border-green-500 px-2 py-0.5 rounded-[1rem] bg-green-500">
@@ -57,21 +57,21 @@ const Highlights = ({ data }) => {
       <div className="border border-gray-800 rounded-[2rem] w-[91rem] h-[27rem] px-8 pb-8 pt-2 bg-transparent shadow-2xl">
         <p className="text-slate-50 py-6">Today's highlights</p>
         <br />
-        <div className="border border-gray-700 rounded-[1rem] bg-transparent w-[15rem] p-4 absolute top-[18.4rem]">
+        <div className="border border-gray-700 rounded-[1rem] backdrop-blur-xl w-[15rem] p-4 absolute top-[18.4rem]">
           <p className="text-slate-50">Humidity</p>
           <div className="flex justify-between items-center pt-6">
             <img className="w-12" alt="weather" src="icons/50d.png" />
             <p className="text-slate-50 text-3xl">{data.main.humidity}%</p>
           </div>
         </div>
-        <div className="border border-gray-700 rounded-[1rem] bg-transparent w-[15rem] p-4 absolute top-[18.4rem] left-[20rem]">
+        <div className="border border-gray-700 rounded-[1rem] backdrop-blur-xl w-[15rem] p-4 absolute top-[18.4rem] left-[20rem]">
           <p className="text-slate-50">Pressure</p>
           <div className="flex justify-between items-center pt-6">
             <img className="w-12" alt="weather" src="icons/50d.png" />
             <p className="text-slate-50 text-3xl">{data.main.pressure}hPa</p>
           </div>
         </div>
-        <div className="border border-gray-700 rounded-[1rem] bg-transparent w-[31rem] p-6 pb-8 absolute top-[7em] left-[37rem]">
+        <div className="border border-gray-700 rounded-[1rem] backdrop-blur-xl w-[31rem] p-6 pb-8 absolute top-[7em] left-[37rem]">
           <p className="text-slate-50">Sunrise & Sunset</p>
           <div className="flex justify-between items-center pt-6">
             <img className="w-12" alt="weather" src="icons/50d.png" />
@@ -96,21 +96,21 @@ const Highlights = ({ data }) => {
             </div>
           </div>
         </div>
-        <div className="border border-gray-700 rounded-[1rem] bg-transparent w-[15rem] p-4 absolute top-[18.4rem] left-[37rem]">
+        <div className="border border-gray-700 rounded-[1rem] backdrop-blur-xl w-[15rem] p-4 absolute top-[18.4rem] left-[37rem]">
           <p className="text-slate-50">Visibility</p>
           <div className="flex justify-between items-center pt-6">
             <img className="w-12" alt="weather" src="icons/50d.png" />
             <p className="text-slate-50 text-3xl">{data.visibility / 1000}Km</p>
           </div>
         </div>
-        <div className="border border-gray-700 rounded-[1rem] bg-transparent w-[15rem] p-4 absolute top-[18.4rem] left-[53rem]">
+        <div className="border border-gray-700 rounded-[1rem] backdrop-blur-xl w-[15rem] p-4 absolute top-[18.4rem] left-[53rem]">
           <p className="text-slate-50">Feels Like</p>
           <div className="flex justify-between items-center pt-6">
             <img className="w-12" alt="weather" src="icons/50d.png" />
             <p className="text-slate-50 text-3xl">{Math.round(data.main.feels_like)}°C</p>
           </div>
         </div>
-        <div className="border border-gray-700 rounded-[1rem] bg-transparent w-[21rem] p-[1.6rem] absolute top-[7rem] left-[70rem]">
+        <div className="border border-gray-700 rounded-[1rem] backdrop-blur-xl w-[21rem] p-[1.6rem] absolute top-[7rem] left-[70rem]">
           <div className="flex justify-between items-center pb-[0.8rem]">
             <p className="text-slate-50">Max Temp</p>
             <p className="text-slate-50">Min Temp</p>
@@ -120,7 +120,7 @@ const Highlights = ({ data }) => {
             <p className="text-slate-50 text-3xl">{Math.round(data.main.temp_min)}°C</p>
           </div>
         </div>
-        <div className="border border-gray-700 rounded-[1rem] bg-transparent w-[21rem] p-4 absolute top-[18.4rem] left-[70rem]">
+        <div className="border border-gray-700 rounded-[1rem] backdrop-blur-xl w-[21rem] p-4 absolute top-[18.4rem] left-[70rem]">
           <p className="text-slate-50">Cloudiness</p>
           <div className="flex justify-between items-center pt-6">
             <img className="w-12" alt="weather" src="icons/50d.png" />
